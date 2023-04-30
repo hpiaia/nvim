160b94e1dff277749df29fc6d41ebd7bedbcc98b
Humberto Piaia <betopiaia@gmail.com>
2023-04-30 12:29:11 -0300

    Fix lsp configuration

--- lua/plugins/modules/lsp.lua
+++ lua/plugins/modules/lsp.lua
@@ -1,62 +1,57 @@
 return {
-	{ "williamboman/mason.nvim" },
-	{
-		"neovim/nvim-lspconfig",
-		config = function()
-			vim.api.nvim_create_autocmd("LspAttach", {
-				group = vim.api.nvim_create_augroup("UserLspConfig", {}),
-				callback = function(ev)
-					local opts = { buffer = ev.buf }
-					vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-					vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
-					vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
-					vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-				end,
-			})
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		config = function()
-			require("mason").setup()
+  { "williamboman/mason.nvim" },
+  { "neovim/nvim-lspconfig" },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason").setup()
 
-			require("mason-lspconfig").setup({
-				ensure_installed = {
-					"lua_ls",
-					"tsserver",
-				},
-			})
+      require("mason-lspconfig").setup({
+        ensure_installed = {
+          "lua_ls",
+          "tsserver",
+          "rust_analyzer",
+        },
+      })
 
-			require("mason-lspconfig").setup_handlers({
-				function(server_name)
-					require("lspconfig")[server_name].setup()
-				end,
+      vim.api.nvim_create_autocmd("LspAttach", {
+        group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+        callback = function(e)
+          local opts = { buffer = e.buf }
+          vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+          vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+          vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+          vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+        end,
+      })
 
-				["lua_ls"] = function(server_name)
-					require("lspconfig")[server_name].setup({
-						settings = {
-							Lua = {
-								format = { enable = false },
-								diagnostics = { globals = { "vim" } },
-							},
-						},
-					})
-				end,
+      require("mason-lspconfig").setup_handlers({
+        function(server_name)
+          require("lspconfig")[server_name].setup({})
+        end,
 
-				["tsserver"] = function(server_name)
-					require("lspconfig")[server_name].setup({
-						init_options = {
-							preferences = { disableSuggestions = true },
-						},
-					})
-				end,
+        ["lua_ls"] = function()
+          require("lspconfig").lua_ls.setup({
+            settings = {
+              Lua = {
+                diagnostics = {
+                  globals = { "vim" },
+                },
+              },
+            },
+          })
+        end,
 
-				["gopls"] = function(server_name)
-					require("lspconfig")[server_name].setup({
-						cmd = { "gopls", "serve" },
-					})
-				end,
-			})
-		end,
-	},
+        ["tsserver"] = function()
+          require("lspconfig").tsserver.setup({
+            init_options = {
+              preferences = {
+                disableSuggestions = true,
+              },
+            },
+          })
+        end,
+      })
+    end,
+  },
 }
