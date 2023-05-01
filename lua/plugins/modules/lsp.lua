e782fa2e1bc33ef132fcfc92e37906a1e2840140
Humberto Piaia <betopiaia@gmail.com>
2023-05-01 19:45:05 -0300

    Format code

--- lua/plugins/modules/lsp.lua
+++ lua/plugins/modules/lsp.lua
@@ -1,57 +1,60 @@
 return {
-  { "williamboman/mason.nvim" },
-  { "neovim/nvim-lspconfig" },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    config = function()
-      require("mason").setup()
+	{ "williamboman/mason.nvim" },
+	{ "neovim/nvim-lspconfig" },
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason").setup()
 
-      require("mason-lspconfig").setup({
-        ensure_installed = {
-          "lua_ls",
-          "tsserver",
-          "rust_analyzer",
-        },
-      })
+			require("mason-lspconfig").setup({
+				ensure_installed = {
+					"lua_ls",
+					"tsserver",
+					"rust_analyzer",
+				},
+			})
 
-      vim.api.nvim_create_autocmd("LspAttach", {
-        group = vim.api.nvim_create_augroup("UserLspConfig", {}),
-        callback = function(e)
-          local opts = { buffer = e.buf }
-          vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-          vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
-          vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
-          vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-        end,
-      })
+			vim.api.nvim_create_autocmd("LspAttach", {
+				group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+				callback = function(e)
+					local opts = { buffer = e.buf }
+					vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+					vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+					vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+					vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+				end,
+			})
 
-      require("mason-lspconfig").setup_handlers({
-        function(server_name)
-          require("lspconfig")[server_name].setup({})
-        end,
+			require("mason-lspconfig").setup_handlers({
+				function(server_name)
+					require("lspconfig")[server_name].setup({})
+				end,
 
-        ["lua_ls"] = function()
-          require("lspconfig").lua_ls.setup({
-            settings = {
-              Lua = {
-                diagnostics = {
-                  globals = { "vim" },
-                },
-              },
-            },
-          })
-        end,
+				["lua_ls"] = function()
+					require("lspconfig").lua_ls.setup({
+						settings = {
+							Lua = {
+								format = {
+									enable = false,
+								},
+								diagnostics = {
+									globals = { "vim" },
+								},
+							},
+						},
+					})
+				end,
 
-        ["tsserver"] = function()
-          require("lspconfig").tsserver.setup({
-            init_options = {
-              preferences = {
-                disableSuggestions = true,
-              },
-            },
-          })
-        end,
-      })
-    end,
-  },
+				["tsserver"] = function()
+					require("lspconfig").tsserver.setup({
+						init_options = {
+							preferences = {
+								disableSuggestions = true,
+							},
+						},
+					})
+				end,
+			})
+		end,
+	},
 }
--- lua/plugins/modules/treesitter.lua
+++ lua/plugins/modules/treesitter.lua
@@ -1,16 +1,16 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  config = function()
-    require("nvim-treesitter.configs").setup({
-      ensure_installed = { "lua", "vim", "vimdoc", "query" },
-      auto_install = true,
-      highlight = {
-        enable = true,
-        additional_vim_regex_highlighting = false,
-      },
-      autotag = {
-        enable = true,
-      },
-    })
-  end,
+	"nvim-treesitter/nvim-treesitter",
+	config = function()
+		require("nvim-treesitter.configs").setup({
+			ensure_installed = { "lua", "vim", "vimdoc", "query" },
+			auto_install = true,
+			highlight = {
+				enable = true,
+				additional_vim_regex_highlighting = false,
+			},
+			autotag = {
+				enable = true,
+			},
+		})
+	end,
 }
