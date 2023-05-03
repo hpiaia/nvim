53011222a7def037abb59941c028c7e42c4c230e
Humberto Piaia <betopiaia@gmail.com>
2023-05-03 09:27:36 -0300

    Hide gitignored from tree

--- lua/plugins/modules/neotree.lua
+++ lua/plugins/modules/neotree.lua
@@ -24,7 +24,6 @@ return {
 				use_libuv_file_watcher = true,
 				filtered_items = {
 					hide_dotfiles = false,
-					hide_gitignored = false,
 					hide_by_name = { "node_modules" },
 					never_show = { ".DS_Store", "thumbs.db" },
 				},
