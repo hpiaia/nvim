de486cae9dc0bc1ee94b951b73d600e1faed3211
Humberto Piaia <betopiaia@gmail.com>
2023-05-11 00:10:01 -0300

    Change jump to jump2d

--- lua/plugins/modules/mini.lua
+++ lua/plugins/modules/mini.lua
@@ -3,7 +3,7 @@ return {
 	config = function()
 		require("mini.ai").setup({})
 		require("mini.comment").setup({})
-		require("mini.jump").setup({})
+		require("mini.jump2d").setup({})
 		require("mini.pairs").setup({})
 		require("mini.surround").setup({})
 	end,
