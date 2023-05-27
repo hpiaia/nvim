1f7c92dde186902b682bc6d745ae6ce7d8ee702b
Humberto Piaia <betopiaia@gmail.com>
2023-05-27 12:46:18 -0300

    Remove guifont option

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -3,4 +3,3 @@ local opt = vim.opt
 opt.clipboard = "unnamedplus" -- sync with system clipboard
 opt.shiftwidth = 2 -- size of an indent
 opt.tabstop = 2 -- number of spaces tabs count for
-opt.guifont = "SFMono Nerd Font:h12" -- setup font for gui vim
