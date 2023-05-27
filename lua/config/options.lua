5bc75e3f16fd165311ad269490cd1f8f9271863b
Humberto Piaia <betopiaia@gmail.com>
2023-05-27 12:07:35 -0300

    Add guifont option

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -3,7 +3,4 @@ local opt = vim.opt
 opt.clipboard = "unnamedplus" -- sync with system clipboard
 opt.shiftwidth = 2 -- size of an indent
 opt.tabstop = 2 -- number of spaces tabs count for
-
--- fix markdown indentation settings
-vim.g.markdown_recommended_style = 0
-vim.o.guifont = "SFMono Nerd Font:h12"
+opt.guifont = "SFMono Nerd Font:h12" -- setup font for gui vim
