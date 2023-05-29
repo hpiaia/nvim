9588532bfe6ad852a03746ca48b5e28f511955e0
Humberto Piaia <betopiaia@gmail.com>
2023-05-29 12:41:08 -0300

    Update lsp and dependencies

--- lua/plugins/modules/lsp.lua
+++ lua/plugins/modules/lsp.lua
@@ -8,31 +8,29 @@ return {
 
 			require("mason-lspconfig").setup({
 				ensure_installed = { "lua_ls", "tsserver", "gopls", "rust_analyzer" },
-			})
-
-			require("mason-lspconfig").setup_handlers({
-				function(server_name)
-					require("lspconfig")[server_name].setup({})
-				end,
-
-				["lua_ls"] = function()
-					require("lspconfig").lua_ls.setup({
-						settings = {
-							Lua = {
-								format = { enable = false },
-								diagnostics = { globals = { "vim" } },
+				automatic_installation = true,
+				handlers = {
+					function(server_name)
+						require("lspconfig")[server_name].setup({})
+					end,
+					["lua_ls"] = function()
+						require("lspconfig").lua_ls.setup({
+							settings = {
+								Lua = {
+									format = { enable = false },
+									diagnostics = { globals = { "vim" } },
+								},
 							},
-						},
-					})
-				end,
-
-				["tsserver"] = function()
-					require("lspconfig").tsserver.setup({
-						init_options = {
-							preferences = { disableSuggestions = true },
-						},
-					})
-				end,
+						})
+					end,
+					["tsserver"] = function()
+						require("lspconfig").tsserver.setup({
+							init_options = {
+								preferences = { disableSuggestions = true },
+							},
+						})
+					end,
+				},
 			})
 		end,
 	},
