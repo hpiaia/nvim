c967a8d6a609b6a3f6443fbe3bb9cad6d1632ff4
Humberto Piaia <betopiaia@gmail.com>
2023-05-29 14:57:29 -0300

    Fix treesitter indent

--- lua/plugins/modules/treesitter.lua
+++ lua/plugins/modules/treesitter.lua
@@ -6,7 +6,10 @@ return {
 			auto_install = true,
 			highlight = {
 				enable = true,
-				additional_vim_regex_highlighting = false,
+				additional_vim_regex_highlighting = true,
+			},
+			indent = {
+				enable = true,
 			},
 		})
 	end,
