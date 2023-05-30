85806f3c907660abe2da3012da22ef7085bc1027
Humberto Piaia <betopiaia@gmail.com>
2023-05-30 15:54:34 -0300

    Update dependencies and customize starter

--- lua/plugins/modules/mini.lua
+++ lua/plugins/modules/mini.lua
@@ -5,12 +5,24 @@ return {
 			mappings = { windows = true },
 			silent = true,
 		})
+
 		require("mini.move").setup({
 			mappings = { left = "H", right = "L", down = "J", up = "K" },
 		})
-		require("mini.starter").setup({})
+
 		require("mini.surround").setup({})
 		require("mini.comment").setup({})
 		require("mini.pairs").setup({})
+
+		local starter = require("mini.starter")
+		starter.setup({
+			items = {
+				starter.sections.recent_files(5, true),
+			},
+			content_hooks = {
+				starter.gen_hook.adding_bullet(),
+				starter.gen_hook.aligning("center", "center"),
+			},
+		})
 	end,
 }
