f5ec5330642a768a188a12fb38d8f8b27647eff6
Humberto Piaia <betopiaia@gmail.com>
2023-06-01 19:50:52 -0300

    Setup dap and go formatting

--- lua/plugins/modules/formatting.lua
+++ lua/plugins/modules/formatting.lua
@@ -6,6 +6,9 @@ return {
 			sources = {
 				null_ls.builtins.formatting.stylua,
 				null_ls.builtins.formatting.prettierd,
+				null_ls.builtins.formatting.gofmt,
+				null_ls.builtins.formatting.goimports,
+				null_ls.builtins.formatting.goimports_reviser,
 			},
 		})
 	end,
