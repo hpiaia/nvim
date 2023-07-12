d56e7ffc87910a0c12d2e390827a36e3cb35f2fa
Humberto Piaia <betopiaia@gmail.com>
2023-07-11 23:29:02 -0300

    Remove mix and update dependencies

--- lua/plugins/modules/lsp.lua
+++ lua/plugins/modules/lsp.lua
@@ -42,7 +42,6 @@ return {
                 sources = {
                     null_ls.builtins.formatting.stylua,
                     null_ls.builtins.formatting.prettierd,
-                    null_ls.builtins.formatting.mix,
                 },
             })
         end,
