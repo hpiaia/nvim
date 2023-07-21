01c3ace239a0ccf80b1d0bd5a2f535ef28fd5053
Humberto Piaia <betopiaia@gmail.com>
2023-07-21 18:33:16 -0300

    Update dependencies and remove null ls

--- lua/plugins/modules/lsp.lua
+++ lua/plugins/modules/lsp.lua
@@ -33,16 +33,4 @@ return {
             })
         end,
     },
-    {
-        "jose-elias-alvarez/null-ls.nvim",
-        config = function()
-            local null_ls = require("null-ls")
-            null_ls.setup({
-                temp_dir = "/tmp",
-                sources = {
-                    null_ls.builtins.formatting.stylua,
-                },
-            })
-        end,
-    },
 }
