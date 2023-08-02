ffc79250c05d323848825f6983584a4cfad9553f
Humberto Piaia <betopiaia@gmail.com>
2023-08-02 15:56:53 -0300

    Update colorscheme

--- lua/plugins/modules/colorscheme.lua
+++ lua/plugins/modules/colorscheme.lua
@@ -5,7 +5,6 @@ return {
         require("catppuccin").setup({
             flavour = "latte",
             transparent_background = true,
-            show_end_of_buffer = true,
             term_colors = true,
             styles = {
                 types = { "italic" },
