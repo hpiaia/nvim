a14c3c1cf914e4e4d0bd9007672fe61f51a5ed1a
Humberto Piaia <betopiaia@gmail.com>
2023-08-02 17:27:09 -0300

    Use neogit

--- lua/plugins/modules/git.lua
+++ lua/plugins/modules/git.lua
@@ -1,5 +1,11 @@
 return {
-    { "tpope/vim-fugitive" },
+    {
+        "NeogitOrg/neogit",
+        config = function()
+            local neogit = require("neogit")
+            neogit.setup({})
+        end,
+    },
     {
         "lewis6991/gitsigns.nvim",
         config = function()
