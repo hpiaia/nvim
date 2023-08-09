a6f495bd7fc46a15af3a1e18c440dd0b9f646b21
Humberto Piaia <betopiaia@gmail.com>
2023-08-08 21:13:22 -0300

    Fix colorscheme on init

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -7,7 +7,7 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup("plugins.modules", {
     install = {
         missing = true,
-        colorscheme = { "catppuccin" },
+        colorscheme = { "tokyonight" },
     },
     performance = {
         rtp = {
