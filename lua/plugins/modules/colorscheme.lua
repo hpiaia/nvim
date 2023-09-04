e47349850606dadd29b17bce759339ff0dcefa4d
Humberto Piaia <betopiaia@gmail.com>
2023-09-04 00:17:51 -0300

    Update colorscheme

--- lua/plugins/modules/colorscheme.lua
+++ lua/plugins/modules/colorscheme.lua
@@ -3,18 +3,13 @@ return {
     config = function()
         require("catppuccin").setup({
             transparent_background = true,
-            show_end_of_buffer = true,
+            custom_highlights = function(colors)
+                return {
+                    CursorLine = { bg = colors.none },
+                }
+            end,
         })
 
         vim.cmd.colorscheme("catppuccin")
-
-        -- enable transparency
-        vim.cmd("hi CursorLine ctermbg=none guibg=none")
-        vim.cmd("hi Normal ctermbg=none guibg=none")
-        vim.cmd("hi SignColumn ctermbg=none guibg=none")
-        vim.cmd("hi NormalNC ctermbg=none guibg=none")
-        vim.cmd("hi MsgArea ctermbg=none guibg=none")
-        vim.cmd("hi TelescopeBorder ctermbg=none guibg=none")
-        vim.cmd("let &fcs='eob: '")
     end,
 }
