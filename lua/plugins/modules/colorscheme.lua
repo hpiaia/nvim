6ca5a21ba5c6f901dceb949f49cdc0abb4038291
Humberto Piaia <betopiaia@gmail.com>
2024-01-22 14:50:04 -0300

    Update dependencies and change colorscheme

--- lua/plugins/modules/colorscheme.lua
+++ lua/plugins/modules/colorscheme.lua
@@ -2,6 +2,12 @@ return {
     "folke/tokyonight.nvim",
     name = "tokyonight",
     config = function()
+        require("tokyonight").setup({
+            style = "night",
+            transparent = true,
+            on_colors = function() end,
+            on_highlights = function() end,
+        })
         vim.cmd.colorscheme("tokyonight")
     end,
 }
